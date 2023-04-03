c8d16da2eaafea5dd5c90c62323dd9b7bdc56aba
Marek Sowinski <sowinmar@gmail.com>
2023-04-03 22:38:59 +0200

    Job done

--- count_elements.rb
+++ count_elements.rb
@@ -1,3 +1,11 @@
 class CountElements
+  attr_reader :array
 
+  def initialize(array)
+    @array = array
+  end
+
+  def perform
+    @array.tally
+  end
 end
--- validate_password.rb
+++ validate_password.rb
@@ -1,3 +1,15 @@
 class ValidatePassword
-
+  attr_reader :password
+  def initialize(password)
+    @password = password
+  end
+ 
+  def perform
+    password.length.between?(6, 24) &&
+      password.match?(/[A-Z]/) &&
+      password.match?(/[a-z]/) &&
+      password.match?(/[0-9]/) &&
+      password.match?(/[!@#$%^&*()_+]/) &&
+      !password.match?(/(.)\1{2}/)
+  end
 end
